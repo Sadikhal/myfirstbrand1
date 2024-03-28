245b347495256620cbfd193fddbb61235478ca25
Sadikhali-pv <122432010+Sadikhali-pv@users.noreply.github.com>
2024-03-28 10:04:16 +0530

    Initial commisst

--- components/constants/Testimonial.js
+++ components/constants/Testimonial.js
@@ -5,18 +5,18 @@ export const Testimonial = [
     id:1,
     name : "Binny",
     brand : "Invex",
-    desc : "Working with Mybrandfirst has been a game-changer for our business. Their strategic approach and attention to detail have significantly boosted our online presence. They truly understand our brand and audience, delivering exceptional results that exceeded our expectations. We highly recommend their services to anyone looking to elevate their digital marketing efforts.",
+    desc : "Working with Mybrandfirst has been a game-changer for our business. Their strategic approach and attention to detail have significantly boosted our online presence. They truly understand our brand and audience, delivering exceptional results that exceeded our expectations. We deeply appreciate their dedicated commitments. We highly recommend their services to anyone looking to elevate their digital marketing efforts.",
   },
   {
     id:1,
     name : "Shafeek",
     brand : "Comfold",
-    desc : "We've partnered with several digital marketing agencies in the past, but none have provided the level of expertise and personalized service like Mybrandfirst. Their team took the time to understand and crafted tailored solutions that drove tangible results. Thanks to their efforts, our online  sales have seen remarkable growth. We're grateful for their dedication and professionalism.",
+    desc : "We've partnered with several digital marketing agencies in the past, but none have provided the level of expertise and personalized service like Mybrandfirst. Their team took the time to understand our unique challenges and crafted tailored solutions that drove tangible results. Thanks to their efforts, our online engagement and sales have seen remarkable growth. We're grateful for their dedication and professionalism. ",
   },
   {
     id:3,
     name : "Nizar",
     brand : "Cbeeso",
-    desc : " I can't speak highly enough about Mybrandfirst. From the initial consultation to the ongoing campaign management, their creative approach, coupled with their in-depth analytics, has helped us achieve remarkable ROI on our digital marketing investments. If you're looking for a partner to take your online presence to the next level, look no further than Mybrandfirst.",
+    desc : "  I can't speak highly enough about Mybrandfirst. From the initial consultation to the ongoing campaign management, they've been a pleasure to work with. Their creative approach, coupled with their in-depth analytics, has helped us achieve remarkable ROI on our digital marketing investments. If you're looking for a reliable partner to take your online presence to the next level, look no further than Mybrandfirst.",
   }
 ]
