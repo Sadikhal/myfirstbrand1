05974367728d7a76bfdc653b880525965e50bf49
Sadikhali-pv <122432010+Sadikhali-pv@users.noreply.github.com>
2024-03-31 18:14:02 +0530

    Initial commisst

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -17,7 +17,7 @@ const page = () => {
       <BlogNav/>
       <ServiceCarousal/>
       <CarouselBlog/>
-      <div  className=' flex flex-col lg:flex-row gap-10  px-8 pl-10 pt-12 items-start'>
+      <div  className=' flex flex-col lg:flex-row gap-10 px-4  md:px-8 pl-10 pt-12 items-start'>
           <div className='flex-1 '>
           {blogNotes.map((item, index) => (
         <BlogBanner key={item.id} item={item} isLast={index === blogNotes.length - 1} />
